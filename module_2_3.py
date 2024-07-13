c28e5038de44760653f649d921c021318babda0e
Reshetkov <alexander.reshetkov.cb@gmail.com>
2024-07-13 21:08:54 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,11 @@
+my_list = [42, 69, 0, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i = 0
+N = len(my_list)
+while i <= N:
+    if (my_list[i]) < 0:
+        break
+    if my_list[i] > 0:
+        print(my_list[i])
+        i += 1
+    else:
+        i += 1
