8e32f6623699f02c8b50ae1f6b98f8333de5c527
Решетков Александр <alexander.reshetkov.cb@gmail.com>
2024-10-02 13:46:17 +0300

    Add files via upload

--- module_10_5.py
+++ module_10_5.py
@@ -19,7 +19,6 @@ if __name__ == '__main__':
     t2 = datetime.datetime.now()
     print(f'{t2 - t1} (линейный)')
 
-if __name__ == '__main__':
     with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
         t3 = datetime.datetime.now()
         pool.map(read_info, filenames)
