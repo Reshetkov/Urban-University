06c07721e1c928ee388aa6ff986dcccecdd4a95f
Решетков Александр <alexander.reshetkov.cb@gmail.com>
2024-10-10 00:14:08 +0300

    Add files via upload

---
+++ module_11_3.py
@@ -0,0 +1,22 @@
+from pprint import pprint
+import numpy as np
+import inspect
+def introspection_info(obj):
+    type_obj = type(obj)
+    attributes = []
+    methods = []
+    for attr_name in dir(obj):
+        if '__'  not in attr_name:
+            attributes.append(attr_name)
+        else:
+            methods.append(attr_name)
+    module = inspect.getmodule(obj)
+    is_float = isinstance(determinant, float)
+
+    return {f'type: {type_obj}, attributes: {attributes}, methods: {methods}, module: {module}, float: {is_float}'}
+
+matrix = np.array([[5, 3, -2], [3, 2, -3], [4, 2, -1]])
+determinant = np.linalg.det(matrix)
+
+determinant_info = introspection_info(determinant)
+pprint(determinant_info)
