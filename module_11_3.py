8d291be029f9325896ad7037edbbcf03d1dc92c2
Решетков Александр <alexander.reshetkov.cb@gmail.com>
2024-10-10 00:19:44 +0300

    Add files via upload

--- module_11_3.py
+++ module_11_3.py
@@ -10,10 +10,12 @@ def introspection_info(obj):
             attributes.append(attr_name)
         else:
             methods.append(attr_name)
-    module = inspect.getmodule(obj)
+    module = __name__
+    module_origin = inspect.getmodule(obj)
     is_float = isinstance(determinant, float)
 
-    return {f'type: {type_obj}, attributes: {attributes}, methods: {methods}, module: {module}, float: {is_float}'}
+    return {f'type: {type_obj}, attributes: {attributes}, methods: {methods}, module: {module}, '
+            f'float: {is_float}, from module: {module_origin}'}
 
 matrix = np.array([[5, 3, -2], [3, 2, -3], [4, 2, -1]])
 determinant = np.linalg.det(matrix)
