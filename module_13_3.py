9e3fce0e86917afda0ee1a23342668c9b80dc87f
Решетков Александр <alexander.reshetkov.cb@gmail.com>
2024-11-11 23:31:43 +0300

    Add files via upload

---
+++ module_13_3.py
@@ -0,0 +1,27 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+bot = Bot(token=)
+dp = Dispatcher()
+
+
+@dp.message(Command('start'))
+async def start(message: Message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+    print('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message()
+async def all_massages(message: Message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+    print('Введите команду /start, чтобы начать общение.')
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
