4a3fe74aecd8d350f5baacc9bdb432ba43a52de0
Решетков Александр <alexander.reshetkov.cb@gmail.com>
2024-11-27 20:12:40 +0300

    Add files via upload

--- crud_functions.py
+++ crud_functions.py
@@ -11,9 +11,17 @@ def initiate_db():
     description TEXT,
     price INTEGER NOT NULL)
     ''')
-    for i in range(1, 5):
-        cursor.execute("INSERT INTO Products (id, title, description, price) VALUES(?, ?, ?, ?)",
-                       (f'{i}', f'Продукт{i}', f'Описание{i}', f'{i * 100}'))
+#    for i in range(1, 5):
+#        cursor.execute("INSERT INTO Products (id, title, description, price) VALUES(?, ?, ?, ?)",
+#                           (f'{i}', f'Продукт{i}', f'Описание{i}', f'{i * 100}'))
+
+    cursor.execute(''' CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    balance INTEGER NOT NULL)
+    ''')
     db.commit()
 
 
@@ -23,6 +31,19 @@ def get_all_products():
     products = cursor.fetchall()
     db.commit()
     return products
+
+
+def add_user(username, email, age):
+    cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES(?, ?, ?, ?)",
+                   (username, email, age, '1000'))
+    db.commit()
+
+
+def is_included(username):
+    cursor.execute("SELECT username FROM Users")
+    users = cursor.fetchall()
+    is_on_the_list = any(user[0] == username for user in users)
+    return is_on_the_list
 
 
 initiate_db()
